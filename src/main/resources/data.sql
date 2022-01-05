94f3a354234da0988fb4a707b541139098c70a26
Mladen Kralev <mladenkralev@gmail.com>
2022-01-05 22:05:59 +0200

    Chaning actually the table i am inserting

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,5 +2,5 @@ INSERT INTO `users` VALUES (0,true,'test@abv.bg','$2a$10$PdLu3gaQSeZNvTJtz5qdbOm
 INSERT INTO `challenges` (`id`, `description`, `end_date`, `occurrences`, `start_date`, `title`) VALUES ('1', 'Read a book every week!', '2022-01-31 00:00:00.000000', 'DAY', '2022-01-05 00:00:00.000000', 'Knowledge is power!');
 INSERT INTO `challenges` (`id`, `description`, `end_date`, `occurrences`, `start_date`, `title`) VALUES ('2', 'Train every day for at least 20 minutes', '2022-01-31 00:00:00.000000', 'DAY', '2022-01-05 00:00:00.000000', 'You are a MACHINE!');
 
-INSERT INTO `db`.`created_by_user_challenges` (`user_id`, `challenge_id`) VALUES ('0', '1');
-INSERT INTO `db`.`created_by_user_challenges` (`user_id`, `challenge_id`) VALUES ('0', '2');
+INSERT INTO `assigned_to_user_challenges` (`user_id`, `challenge_id`) VALUES ('0', '1');
+INSERT INTO `assigned_to_user_challenges` (`user_id`, `challenge_id`) VALUES ('0', '2');
