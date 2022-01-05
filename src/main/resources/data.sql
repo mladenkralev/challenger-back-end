f188d9881e2a7484ea25a2eaf5b15815631544ff
Mladen Kralev <mladenkralev@gmail.com>
2022-01-05 21:23:51 +0200

    Connecting challenges to user

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO `users` VALUES (0,true,'test@abv.bg','$2a$10$PdLu3gaQSeZNvTJtz5qdbOmsTrJQ4bTHIr40mNgc//Fa8OW7b2Vla','ROLE_ADMIN','mladen');
 INSERT INTO `challenges` (`id`, `description`, `end_date`, `occurrences`, `start_date`, `title`) VALUES ('1', 'Read a book every week!', '2022-01-31 00:00:00.000000', 'DAY', '2022-01-05 00:00:00.000000', 'Knowledge is power!');
 INSERT INTO `challenges` (`id`, `description`, `end_date`, `occurrences`, `start_date`, `title`) VALUES ('2', 'Train every day for at least 20 minutes', '2022-01-31 00:00:00.000000', 'DAY', '2022-01-05 00:00:00.000000', 'You are a MACHINE!');
+
+INSERT INTO `db`.`created_by_user_challenges` (`user_id`, `challenge_id`) VALUES ('0', '1');
+INSERT INTO `db`.`created_by_user_challenges` (`user_id`, `challenge_id`) VALUES ('0', '2');
