755aa5a4bb724e11c53723cb743a449c583435cb
Mladen Kralev <mladenkralev@gmail.com>
2022-01-05 20:06:21 +0200

    Adding two challenges

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO `users` VALUES (0,true,'test@abv.bg','$2a$10$PdLu3gaQSeZNvTJtz5qdbOmsTrJQ4bTHIr40mNgc//Fa8OW7b2Vla','ROLE_ADMIN','mladen');
+INSERT INTO `challenges` (`id`, `description`, `end_date`, `occurrences`, `start_date`, `title`) VALUES ('1', 'Read a book every week!', '2022-01-31 00:00:00.000000', 'DAY', '2022-01-05 00:00:00.000000', 'Knowledge is power!');
+INSERT INTO `challenges` (`id`, `description`, `end_date`, `occurrences`, `start_date`, `title`) VALUES ('2', 'Train every day for at least 20 minutes', '2022-01-31 00:00:00.000000', 'DAY', '2022-01-05 00:00:00.000000', 'You are a MACHINE!');
